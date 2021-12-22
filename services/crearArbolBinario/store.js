2cf362869f60d87c0e73377e7bdfd056009f8222
ingenierocesar <cesarago852@gmail.com>
2021-12-22 05:31:18 -0500

    Ajuste control de conexion/desconexion servidor redis

--- services/crearArbolBinario/store.js
+++ services/crearArbolBinario/store.js
@@ -1,58 +1,85 @@
 /*
 La capa de almacenamiento esta adaptada en caso de existir un servicio de base de datos con redis. 
 */
-const redis = require('redis');
+const Redis = require('ioredis');
 const config = require('../../config/index');
 let statusRedisConexion = false;
 let client;
+let arbolBinario = [];
 
-// const redisClient = redis.createClient(6379,'127.0.0.1');
-// redisClient.on('connection', () => { 
-//     console.info('Redis connected!');
-//   });
-
-if(config.redis_port.length > 0 && config.redis_url.length > 0){
-    try {
-        //creamos un cliente
-        statusRedisConexion = true;
-        (async () => {
-          client = redis.createClient();
-        
-          client.on('error', (err) => {
-              console.log('Redis Client Error', err);
-              statusRedisConexion = false;
+const iniciarConexion = async () => {
+    if(config.redis_port.length > 0 && config.redis_url.length > 0){
+        try {
+            //creamos un cliente
+            statusRedisConexion = true;
+            client = new Redis({
+                port: config.redis_port,
+                host: config.redis_url,
+                maxRetriesPerRequest : 1 , 
+                retryStrategy(times) {
+                    const delay = Math.min(times * 50000, 20000);
+                    return false;
+                  },
             });
-        
-          await client.connect();
 
-        })();
-    } catch (error) {
-        console.log("error",error)
+        
+            client.on('error', async (err) => {
+                console.log(err);
+                statusRedisConexion = false;                
+            });
+       
+
+            // client.disconnect();
+    
+        } catch (error) {
+            // console.log(error)
+        }
     }
 }
 
-var arbolBinario = [];
-
+/*
+Query oara agregar arbol
+*/
 const crearArbolBinario = async ( arrayArbolBinario ) => {
-    statusRedisConexion == false ?
-        arbolBinario = arrayArbolBinario
-        :
-        await client.set('arbol_binario', JSON.stringify(arrayArbolBinario))
+    try {
+        iniciarConexion();
+        console.log(statusRedisConexion);
+
+        statusRedisConexion == false ?
+            arbolBinario = arrayArbolBinario
+            :
+            await client.set('arbol_binario', JSON.stringify(arrayArbolBinario))
+
+    } catch (error) {
+        // console.log(error)
+    }
+
 }
 
 const obtenerArbolNBinario = async () => {
+    try {
+        iniciarConexion();
+
     statusRedisConexion == false ?
         arbolBinario
         :
         JSON.parse(await client.get('arbol_binario'));
-                
+    } catch (error) {
+        // console.log(error)
+    } 
 }
 
 const actualizarArbolBinario = async ( arrayArbolBinario ) => {
+    try {
+        iniciarConexion();
+
     statusRedisConexion == false ?
         arbolBinario = arrayArbolBinario
         :
         await client.set('arbol_binario', JSON.stringify(arrayArbolBinario))
+    } catch (error) {
+        // console.log(error)
+    }
 }
 
 module.exports = {
